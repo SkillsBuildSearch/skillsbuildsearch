b2eaab0fb04a80fde8ba225ea1e9fbabc985aceb
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2022-11-21 14:01:15 +0000

    Removes HelloWorld component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,8 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <TheWelcome />
   </main>
