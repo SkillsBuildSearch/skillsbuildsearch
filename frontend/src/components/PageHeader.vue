b8bba88fd0d9f618b7daa9fe770bb8b0da3cd706
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2022-11-25 12:22:27 +0000

    Adds IBM logo

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1 class="text-center mt-2">SkillsBuildSearch</h1>
+  <div class="row justify-content-center align-items-center my-3">
+    <div class="col-auto">
+      <img style="width: 150px" src="img/ibm.png" />
+    </div>
+    <div class="col-auto">
+      <h1>SkillsBuildSearch</h1>
+    </div>
+  </div>
 </template>
