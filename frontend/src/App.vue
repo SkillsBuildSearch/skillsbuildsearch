696f2a9f481b8569cef6f6c98973b159aad7a3b7
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2022-11-25 12:23:37 +0000

    Adds search results

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,34 @@
 <script setup>
 import PageHeader from "./components/PageHeader.vue";
 import SearchBox from "./components/SearchBox.vue";
+import SearchResults from "./components/SearchResults.vue";
 </script>
 
 <template>
   <main>
     <div class="container">
       <PageHeader />
-      <SearchBox />
+      <SearchBox @search="toggleShow" />
+      <SearchResults v-if="showResults" />
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showResults: false,
+    };
+  },
+  methods: {
+    toggleShow(result) {
+      if (result) {
+        this.showResults = true;
+      } else {
+        this.showResults = false;
+      }
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/SearchResults.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="result in searchResults" :key="result.link">
+    <h2>
+      <a :href="result.link">{{ result.title }}</a>
+    </h2>
+    <p>{{ result.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchResults: [
+        {
+          title: "AI course",
+          desc: "The best AI course ever",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
