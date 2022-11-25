3e8c9439b18a46295c784b46784f9412a84e5517
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2022-11-25 11:24:44 +0000

    Adds search button

--- frontend/src/components/SearchBox.vue
+++ frontend/src/components/SearchBox.vue
@@ -1,10 +1,19 @@
 <template>
-  <input type="search" placeholder="Input your course here..." />
+  <div class="input-group mb-3">
+    <input
+      type="search"
+      placeholder="Input your course here..."
+      aria-label="Course description"
+      aria-describedby="search-button"
+    />
+    <button class="btn btn-outline-primary" type="button" id="search-button">
+      Search
+    </button>
+  </div>
 </template>
 
 <style scoped>
 input {
-  width: 100%;
   height: 50px;
 }
 </style>
