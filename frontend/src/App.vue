9996b85e708eb9704aadc1e9a2463eab3cbad163
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2022-12-01 11:00:50 +0000

    Adds checkboxes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,8 +8,30 @@ import SearchResults from "./components/SearchResults.vue";
   <main>
     <div class="container">
       <PageHeader />
-      <SearchBox @search="toggleShow" />
-      <SearchResults v-if="showResults" />
+      <div class="row">
+        <div class="col">
+          <SearchBox @search="toggleShow" />
+          <SearchResults v-if="showResults" />
+        </div>
+        <div class="col-auto bg-ligh">
+          <h2>Course categories</h2>
+          <div
+            v-for="category in Object.entries(categories)"
+            :key="category[0]"
+            class="form-check"
+          >
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="categories[category[0]]"
+              :id="category[0] + 'Input'"
+            />
+            <label class="form-check-label" :for="category + 'Input'">
+              {{ category[0] }}
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -19,6 +41,18 @@ export default {
   data() {
     return {
       showResults: false,
+      categories: {
+        "Artificial Intelligence": false,
+        Capstone: false,
+        "Data Science": false,
+        "IBM Automation": false,
+        "IBM Cloud": false,
+        "IBM Engineering": false,
+        "IBM Security": false,
+        "IBM Z": false,
+        "Red Hat Academy": false,
+        "IBM Quantum": false,
+      },
     };
   },
   methods: {
