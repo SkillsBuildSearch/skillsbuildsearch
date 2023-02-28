f81d22a59aa011c1c0e5b5dd1b4aa50f02435108
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2023-02-28 14:40:19 +0000

    Adds sign up popup

---
+++ frontend/src/components/SignUpBox.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class="modal fade"
+    id="signUpModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">
+            Hey, you - sign up to SkillsBuild!
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            @click="close()"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          Before you start any of the courses, you need to get your IBM
+          SkillsBuild account here.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="close()">
+            Close
+          </button>
+          <button @click="signUp()" type="button" class="btn btn-primary">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Modal } from "bootstrap";
+
+export default {
+  methods: {
+    saveCookie() {
+      document.cookie = "popup=false; SameSite=Strict; Secure";
+    },
+    close() {
+      this.saveCookie();
+      Modal.getInstance(document.getElementById("signUpModal")).hide();
+    },
+    getCookie() {
+      // Returns true if the popup should be shown.
+      return (
+        "false" !==
+        document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("popup="))
+          ?.split("=")[1]
+      );
+    },
+    signUp() {
+      this.close();
+      window.location.assign("https://skillsbuild.org");
+    },
+  },
+  mounted() {
+    const signUpModal = new Modal("#signUpModal", {
+      keyboard: false,
+    });
+    if (this.getCookie()) {
+      signUpModal.show();
+    }
+  },
+};
+</script>
