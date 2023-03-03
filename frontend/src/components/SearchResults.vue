9a5e9fd3776faff29ccbc42a20f7c8fccbf04c04
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2023-03-03 12:48:34 +0000

    Feat: adds pagination to frontend

--- frontend/src/components/SearchResults.vue
+++ frontend/src/components/SearchResults.vue
@@ -1,16 +1,30 @@
 <template>
-  <div v-for="result in results" :key="result.link">
-    <h2>
-      <a :href="result.Link">{{ result.Title }}</a>
-    </h2>
-    <span
-      v-for="topic in result.Topic.split(', ')"
-      :key="topic"
-      class="badge bg-secondary me-1"
+  <div>
+    <div v-for="result in results" :key="result.link">
+      <h2>
+        <a :href="result.Link">{{ result.Title }}</a>
+      </h2>
+      <span
+        v-for="topic in result.Topic.split(', ')"
+        :key="topic"
+        class="badge bg-secondary me-1"
+      >
+        {{ topic }}
+      </span>
+      <p>{{ result.Description_short }}</p>
+    </div>
+    <div
+      class="d-flex justify-content-center"
+      v-if="results.length < totalResults && results.length"
     >
-      {{ topic }}
-    </span>
-    <p>{{ result.Description_short }}</p>
+      <button
+        @click="$emit('loadMore')"
+        type="button"
+        class="btn btn-outline-primary mx-auto"
+      >
+        Load more...
+      </button>
+    </div>
   </div>
 </template>
 
@@ -18,6 +32,7 @@
 export default {
   props: {
     results: Array,
+    totalResults: Number,
   },
 };
 </script>
