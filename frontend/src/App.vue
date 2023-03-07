b28beecb8b240685078ce5a7b3371f98e2d6e5b9
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2023-03-07 13:21:43 +0000

    Feat: adds client side error handling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@ import PageHeader from "./components/PageHeader.vue";
 import SearchBox from "./components/SearchBox.vue";
 import SearchResults from "./components/SearchResults.vue";
 import SignUpBox from "./components/SignUpBox.vue";
+import ErrorAlert from "./components/ErrorAlert.vue";
 </script>
 
 <template>
@@ -11,7 +12,16 @@ import SignUpBox from "./components/SignUpBox.vue";
       <PageHeader />
       <div class="row">
         <div class="col">
-          <SearchBox @search="loadResults" />
+          <SearchBox
+            @search="loadResults"
+            @error="generateError($event[0], $event[1])"
+          />
+          <ErrorAlert
+            v-for="error in errorMessages"
+            :message="error.message"
+            :code="error.code"
+            :key="error.id"
+          ></ErrorAlert>
           <SearchResults
             :results="results"
             :total-results="maxResults"
@@ -50,6 +60,7 @@ export default {
     SearchBox,
     SearchResults,
     SignUpBox,
+    ErrorAlert,
   },
   data() {
     return {
@@ -59,11 +70,18 @@ export default {
       maxResults: 20,
       lastSearch: "",
       loading: false,
+      errorMessages: [],
     };
   },
   methods: {
     loadResults(text, offset, append) {
       this.loading = true;
+      this.clearErrors();
+
+      // If categories haven't loaded, try to load them again when a search is attempted.
+      if (Object.entries(this.categories).length == 0) {
+        this.loadCategories();
+      }
 
       let uri = `http://localhost:5001/api/v1/search?text=${encodeURIComponent(
         text
@@ -83,39 +101,66 @@ export default {
 
       const searchRequest = new Request(uri);
 
-      fetch(searchRequest).then((response) => {
-        if (!response.ok) {
-          this.loading = false;
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        response.json().then((data) => {
-          if (append) {
-            this.results = this.results.concat(data);
-          } else {
-            this.results = data;
+      fetch(searchRequest)
+        .then((response) => {
+          if (!response.ok) {
+            this.loading = false;
+            this.generateError("Server error. Please try again later.", 1);
           }
+
+          response.json().then((data) => {
+            if (append) {
+              this.results = this.results.concat(data);
+            } else {
+              this.results = data;
+            }
+            this.loading = false;
+            this.clearErrors();
+            this.lastSearch = text;
+          });
+        })
+        .catch((error) => {
           this.loading = false;
-          this.lastSearch = text;
+          this.generateError(
+            "Failed to load search results, as the server is offline. Please try again later.",
+            1
+          );
         });
-      });
     },
     loadCategories() {
       const searchRequest = new Request(
         "http://localhost:5001/api/v1/categories/"
       );
 
-      fetch(searchRequest).then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        response.json().then((data) => {
-          for (const value of data) {
-            this.categories[value] = true;
+      fetch(searchRequest)
+        .then((response) => {
+          if (!response.ok) {
+            this.generateError("Server error. Please try again later.", 1);
           }
+
+          response.json().then((data) => {
+            for (const value of data) {
+              this.categories[value] = true;
+            }
+            this.clearErrors();
+          });
+        })
+        .catch(() => {
+          this.generateError(
+            "Failed to load categories, as the server is offline. Please try again later.",
+            1
+          );
         });
+    },
+    generateError(message, code) {
+      this.errorMessages.push({
+        id: Date.now(),
+        message: message,
+        code: code,
       });
+    },
+    clearErrors() {
+      this.errorMessages = [];
     },
   },
   mounted() {
---
+++ frontend/src/components/ErrorAlert.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Alert } from "bootstrap";
+</script>
+
+<template>
+  <div
+    id="alertInstance"
+    class="alert alert-dismissible fade show"
+    :class="{ 'alert-danger': code == 1, 'alert-warning': code == 2 }"
+    role="alert"
+  >
+    <strong>Error</strong>
+    {{ message }}
+    <!--     <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+    code: Number,
+  },
+  mounted() {
+    setTimeout(() => {
+      const alert = Alert.getOrCreateInstance("#alertInstance");
+      alert.close();
+    }, 5000);
+  },
+};
+</script>
