6cc39b3ebf8e5bccedc88cc3ae5a9ed9ad6695d6
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2023-03-12 12:25:19 +0000

    Creates initial frontend tests for ST05

--- frontend/src/components/SearchBox.vue
+++ frontend/src/components/SearchBox.vue
@@ -9,11 +9,13 @@
       @keyup.enter.prevent="submit"
       maxlength="1000"
       v-model="searchText"
+      ref="searchInput"
     />
     <button
       @click="toggleRecord()"
       class="btn btn-outline btn-outline-secondary"
       type="button"
+      ref="voiceButton"
     >
       <i
         class="bi"
